31dba33e7e9e8870fee66ea14c87e64461e9cbcc
PrasananTrainer03 <PrasananTrainer03>
2023-01-02 17:00:53 +0530

    code

---
+++ Day12_3_Login/ServletDemos/src/main/java/com/java/servlets/EmployDaoImpl.java
@@ -0,0 +1,83 @@
+package com.java.servlets;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployDaoImpl implements EmployDAO {
+
+	Connection connection;
+	PreparedStatement pst;
+	
+	@Override
+	public List<Employ> showEmployDao() throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select * from Employ";
+		pst = connection.prepareStatement(cmd);
+		ResultSet rs = pst.executeQuery();
+		List<Employ> employList = new ArrayList<Employ>();
+		Employ employ = null;
+		while(rs.next()) {
+			employ = new Employ();
+			employ.setEmpno(rs.getInt("empno"));
+			employ.setName(rs.getString("name"));
+			employ.setDept(rs.getString("dept"));
+			employ.setDesig(rs.getString("desig"));
+			employ.setBasic(rs.getInt("basic"));
+			employList.add(employ);
+		}
+		connection.close();
+		pst.close();
+		return employList;
+	}
+
+	@Override
+	public Employ searchEmployDao(int empno) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select * from Employ WHERE Empno=?";
+		pst = connection.prepareStatement(cmd);
+		pst.setInt(1, empno);
+		ResultSet rs = pst.executeQuery();
+		Employ employ = null;
+		if (rs.next()) {
+			employ = new Employ();
+			employ.setEmpno(rs.getInt("empno"));
+			employ.setName(rs.getString("name"));
+			employ.setDept(rs.getString("dept"));
+			employ.setDesig(rs.getString("desig"));
+			employ.setBasic(rs.getInt("basic"));
+		}
+		return employ;
+	}
+
+	@Override
+	public String addEmployDao(Employ employ) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "insert into Employ(name,dept,desig,basic) "
+				+ " values(?,?,?,?)";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, employ.getName());
+		pst.setString(2, employ.getDept());
+		pst.setString(3, employ.getDesig());
+		pst.setInt(4, employ.getBasic());
+		pst.executeUpdate();
+		return "Record Inserted...";
+
+	}
+
+	@Override
+	public int authenticate(String user, String pwd) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select count(*) cnt from login where userName=? AND passcode=?";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, user);
+		pst.setString(2, pwd);
+		ResultSet rs = pst.executeQuery();
+		rs.next();
+		int count = rs.getInt("cnt");
+		return count;
+	}
+}
