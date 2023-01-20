d2e5e8502b5b0310b76339cf3d2853f87883ac40
PrasananTrainer03 <PrasananTrainer03>
2023-01-20 09:26:30 +0530

    code

---
+++ Day24_p1/Wallet_Insert.sql
@@ -0,0 +1,8 @@
+
+Insert into wallet(Cus_ID,Wal_Source,Wal_Amount)
+values(4,'PAYTM',85533),
+(4,'CREDIT_CARD',48222),
+(4,'DEBIT_CARD',48224),
+(5,'PAYTM',90333),
+(5,'CREDIT_CARD',75332),
+(5,'DEBIT_CARD',83322);
